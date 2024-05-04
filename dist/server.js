ec577307abeabb00d1740ac036c140bc01bc5761
mahabubsaki <md929820@gmail.com>
2024-05-04 19:23:16 +0600

    prisma added

---
+++ dist/server.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const db_config_1 = __importDefault(require("./configs/db/db.config"));
+const app_1 = __importDefault(require("./app"));
+const env_config_1 = __importDefault(require("./configs/env/env.config"));
+let server;
+process.on('uncaughtException', (err) => {
+    console.log(err);
+    if (server)
+        server.close(() => process.exit(1));
+    else
+        process.exit(1);
+});
+async function startServer() {
+    try {
+        await db_config_1.default.$connect();
+        server = app_1.default.listen(env_config_1.default.port, () => {
+            console.log(`Server is running on port ${env_config_1.default.port}`);
+        });
+    }
+    catch (err) {
+        console.log(err);
+    }
+    process.on('unhandledRejection', (err) => {
+        console.log(err);
+        if (server)
+            server.close(() => process.exit(1));
+        else
+            process.exit(1);
+    });
+}
+startServer();
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received');
+    if (server)
+        server.close(() => process.exit(1));
+    else
+        process.exit(1);
+});
---
+++ src/server.ts
@@ -0,0 +1,39 @@
+import { Server } from 'http';
+import prisma from './configs/db/db.config';
+import app from './app';
+import envs from './configs/env/env.config';
+let server: Server;
+
+process.on('uncaughtException', (err) => {
+    console.log(err);
+    if (server) server.close(() => process.exit(1));
+    else process.exit(1);
+});
+
+async function startServer() {
+
+    try {
+        await prisma.$connect();
+        server = app.listen(envs.port, () => {
+            console.log(`Server is running on port ${envs.port}`);
+
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
+
+    process.on('unhandledRejection', (err) => {
+        console.log(err);
+        if (server) server.close(() => process.exit(1));
+        else process.exit(1);
+    });
+}
+
+startServer();
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received');
+    if (server) server.close(() => process.exit(1));
+    else process.exit(1);
+});
