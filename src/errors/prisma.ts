4924cacf932a395c65fb0ece3c252cd2a616e902
mahabubsaki <md929820@gmail.com>
2024-05-15 20:53:41 +0600

    check

--- src/errors/prisma.ts
+++ src/errors/prisma.ts
@@ -3,7 +3,7 @@ import { Prisma } from "@prisma/client";
 const prismaErrorHandler = (code: Prisma.PrismaClientKnownRequestError) => {
     let message = '';
 
-    console.log({ prismaCode: code.code });
+    console.log({ prismaCode: code.code }, 'prismaErrorHandler');
     switch (code.code) {
         case 'P2002':
             message = 'There is a unique constraint violation, a new user cannot be created with this email';
